f6da436c68270631da75e71360dffbb7922ef5a1
leehx78 <leehx78@smartscore.kr>
2025-09-05 11:42:49 +0900

    scss fixed

--- src/components/unit/BottomNavi.vue
+++ src/components/unit/BottomNavi.vue
@@ -1,5 +1,12 @@
 <template>
-  <div class="btn-back" @click="goBack">뒤로</div>
+  <div class="bottom-navi">
+    <div class="btn-back" @click="goBack">
+      <i class="ico-arrow-prev"/>
+    </div>
+    <div class="btn-reload" @click="reload">
+      <i class="ico-reload" ref="reloadIcon"/>
+    </div>
+  </div>
 </template>
 <script>
 export default {
@@ -7,16 +14,33 @@ export default {
 }
 </script>
 <script setup>
+import {ref, onMounted} from "vue";
+import { gsap } from "gsap";
+
+const reloadIcon = ref(null);
 const goBack = () => {
   history.back();
 }
+const reload = () => {
+  gsap.to(reloadIcon.value, {
+    rotation: 360,
+    duration: 0.5,
+    ease: "power1.inOut",
+    onComplete: () => {
+      location.reload();
+    }
+  });
+};
 </script>
 <style lang="scss" scoped>
 @use '@/assets/scss/variable' as *;
-.btn-back {
-  position: fixed; bottom: 20px; left: 20px; z-index: 10;
-  display: inline-flex; justify-content: center; align-items: center;
-  width: 60px; height: 60px; background-color: rgba(0,0,0,0.5); border-radius: 50%;
-  color: $color-white; cursor: pointer;
+.bottom-navi {
+  position: fixed; bottom: 20px; left: 20px; z-index: 10; display: flex; gap: 10px;
+  [class^='btn-'] {
+    display: inline-flex; justify-content: center; align-items: center;
+    width: 60px; height: 60px; background-color: rgba(0,0,0,0.5); border-radius: 50%;
+    color: $color-white; cursor: pointer;
+  }
+  [class^='ico-'] {filter: $ico-color-white; transform-origin: center;}
 }
 </style>
