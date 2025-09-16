a9375b02d1de20eee76d3bed5269769264af59e1
leehx78 <leehx78@smartscore.kr>
2025-09-16 16:26:07 +0900

    app fixed

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,10 @@
 <template>
-  <div id="app">
-    <router-view/>
-  </div>
+  <router-view/>
 </template>
 
 <script setup>
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
+#app { overflow: hidden; display: flex; flex-direction: column;}
 </style>
