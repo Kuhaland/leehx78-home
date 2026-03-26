60e7a6ce829c9fbd7de54a4dff845621d361ef54
leehx78 <leehx78@smartscore.kr>
2026-03-26 16:55:09 +0900

    vite.config fixed

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
 
+
 export default defineConfig(({ command }) => ({
   plugins: [vue()],
   base: command === 'serve' ? '/' : '/leehx78-home/',
